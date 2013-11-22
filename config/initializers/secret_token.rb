0c0893107499b23eaa4b35927d4c4eec4d5778f1
Gerrit Wanderer <gerrit@the-wanderer.de>
2013-11-22 17:18:33 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pionira::Application.config.secret_key_base = '7f9d60e60bc5664d04df7925b425032a2c17465a75c9ea5c5c8803f826aa7d45a01a3370a92914f166579ebaeac3f5c80916dedea5a9475f16e597d7b56fd78c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pionira::Application.config.session_store :cookie_store, key: '_pionira_session'
