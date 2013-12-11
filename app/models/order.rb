88ec2db5bd6953034c320616183a51ae998f43ea
Gerrit Wanderer <gerrit@the-wanderer.de>
2013-12-11 12:07:28 +0100

    Add Validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,9 @@
 class Order < ActiveRecord::Base
   has_many :images, :dependent => :destroy
   accepts_nested_attributes_for :images
+  
   validates_associated :images
+  validates :email, presence: true, email: true
 
   before_create :generate_security_key
   after_create :send_new_order_mail
